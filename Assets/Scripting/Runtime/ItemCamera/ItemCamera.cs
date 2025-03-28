5cc4333a0fb8583ddba3e44f33f480be9dac97a5
PosimAndrzej <posimandrzej@gmail.com>
2025-03-28 16:55:38 +0100

    Working on Animation

--- Assets/Scripting/Runtime/ItemCamera/ItemCamera.cs
+++ Assets/Scripting/Runtime/ItemCamera/ItemCamera.cs
@@ -1,13 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class ItemCamera : MonoBehaviour
 {
     [SerializeField] private GameObject camera;
     [SerializeField] private Item itemCamera;
-    
+    [SerializeField] private RigBuilder RigBuilder;
+    [SerializeField] private Rig handMover;
+    private float _handMoverWeightHelp = 0.0f;
     void Start()
     {
-        
+
     }
 
     
@@ -17,11 +21,31 @@ public class ItemCamera : MonoBehaviour
         {
             ChangeCameraEnable();
         }
-        
+        if (camera.activeSelf && handMover.weight != 1.0f)
+        {
+            _handMoverWeightHelp += 0.02f;
+            handMover.weight = _handMoverWeightHelp;
+        }
+        else if(!camera.activeSelf && handMover.weight != 0.0f)
+        {
+            _handMoverWeightHelp -= 0.02f;
+            handMover.weight = _handMoverWeightHelp;
+        }
+        else if(_handMoverWeightHelp <= 0.0f)
+        {
+            _handMoverWeightHelp = -0.02f;
+            //RigBuilder.enabled = false;
+        }
+        else if (_handMoverWeightHelp >= 1.0f)
+        {
+            
+            _handMoverWeightHelp = 1.02f;
+        }
     }
 
     private void ChangeCameraEnable()
     {
+        //RigBuilder.enabled = true;
         camera.SetActive(!camera.activeSelf);
     }
 
