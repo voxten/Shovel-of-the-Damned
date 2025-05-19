229a888cea81697d7136f524b98d1f53e581a1e3
voxten <voxterman@gmail.com>
2025-05-19 19:30:42 +0200

    Added and modified item icons and also added few new sounds

--- Assets/Scripting/Runtime/Menu/PlayButton.cs
+++ Assets/Scripting/Runtime/Menu/PlayButton.cs
@@ -1,10 +1,30 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class PlayButton : ExtendedButton
 {
+    [SerializeField] private AudioSource menuMusic;
+    
     protected override void Submit()
     {
         SceneLoader.SceneEvents.AnimateLoadScene("CarScene");
+        StartCoroutine(FadeOutAndLoadScene());
+    }
+
+    private IEnumerator FadeOutAndLoadScene()
+    {
+        float duration = 2f;
+        float startVolume = menuMusic.volume;
+
+        float time = 0f;
+        while (time < duration)
+        {
+            menuMusic.volume = Mathf.Lerp(startVolume, 0f, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        menuMusic.volume = 0f;
     }
 }
