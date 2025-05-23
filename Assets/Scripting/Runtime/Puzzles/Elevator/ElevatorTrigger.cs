d00942c3580d93b65ed7f19c225c8842ba8e3fca
PosimAndrzej <posimandrzej@gmail.com>
2025-05-23 23:51:19 +0200

    Update ElevatorTrigger.cs

--- Assets/Scripting/Runtime/Puzzles/Elevator/ElevatorTrigger.cs
+++ Assets/Scripting/Runtime/Puzzles/Elevator/ElevatorTrigger.cs
@@ -1,15 +1,31 @@
-using System;
 using UnityEngine;
 
 public class ElevatorTrigger : MonoBehaviour
 {
     private bool _isTriggered;
-    
+    private Inventory _FPC;
+    private float _lastY;
+
+    private void Start()
+    {
+        _FPC = FindAnyObjectByType<Inventory>();
+        _lastY = transform.position.y;
+    }
+
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             _isTriggered = true;
+
+            float currentY = transform.position.y;
+            float deltaY = currentY - _lastY;
+
+            // Przesuwamy gracza o tyle, o ile przemieœci³a siê platforma w osi Y
+            Vector3 playerPos = _FPC.transform.position;
+            _FPC.transform.position = new Vector3(playerPos.x, playerPos.y + deltaY, playerPos.z);
+
+            _lastY = currentY;
         }
     }
 
