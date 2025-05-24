9a036aa265d463182c0986b99836301326cb7fb3
voxten <voxterman@gmail.com>
2025-05-24 17:25:13 +0200

    Added saving circle

--- Assets/Scripting/Runtime/Menu/LoadButton.cs
+++ Assets/Scripting/Runtime/Menu/LoadButton.cs
@@ -5,6 +5,7 @@ public class LoadButton : ExtendedButton
     protected override void Submit()
     {
         base.Submit();
+        Time.timeScale = 1f;
         SavingSystem.SavingSystemEvents.Load();
     }
 }
